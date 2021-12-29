3c9dc26ea227cb5dbf3a332b62d6425eb9576ebe
pemba1s1 <norsangpemba@gmail.com>
2021-12-29 14:36:00 +0545

    login form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div>
+        <div className='position-fixed bottom-0 w-100'>
             <p className='text-center'>Copyright &copy; QuickFox 2021</p>
         </div>
     )
--- src/pages/Auth/Login/Login.js
+++ src/pages/Auth/Login/Login.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
     return (
-        <div>
-            Log In
+        <form className='login-form mx-auto mt-5 p-3 pb-1 border'>
+        <p className='text-center'>Welcome back!</p>
+        <p className='text-center'>Sign in to ask or answer questions and unlock all features.</p>
+        <div className="mb-3">
+            <label for="exampleInputEmail1" className="form-label">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+            <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+            <div class="invalid-feedback">
+        Please choose a username.
+      </div>
         </div>
+        <div className="mb-3">
+            <label for="exampleInputPassword1" className="form-label">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" required/>
+        </div>
+        <button type="submit" className="btn btn-primary w-100">Log In</button>
+        <p className='border-top mt-3 text-center pt-2'>New to QuickFlow? <Link to='/signup'>Create an account.</Link></p>
+        </form>
     )
 }
