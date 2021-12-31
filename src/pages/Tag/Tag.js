f89b80e4c90a7ad628d0fe65bb0b8e2f13bbb77f
pemba1s1 <norsangpemba@gmail.com>
2021-12-31 14:31:01 +0545

    vertical navbar

---
+++ src/pages/Tag/Tag.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import SecondaryLayout from '../../layout/SecondaryLayout'
+
+export default function Tag() {
+    return (
+        <>
+            <SecondaryLayout>
+            <div className='ms-auto px-3 py-4 content border'>
+                Tags
+            </div>
+            </SecondaryLayout>
+        </>
+    )
+}
--- src/router/Router.js
+++ src/router/Router.js
@@ -2,9 +2,10 @@ import {Switch,Route} from 'react-router-dom'
 import Home from "../pages/Home/Home"
 import Login from "../pages/Auth/Login/Login"
 import Signup from "../pages/Auth/Signup/Signup"
-import Category from "../pages/Category/Category"
+import Tag from "../pages/Tag/Tag"
 import NotFound from '../pages/404/404'
 import Recover from '../pages/Auth/Login/Recover'
+import Question from '../pages/Question/Question'
 
 export const Router = () => {
     return(
@@ -12,8 +13,9 @@ export const Router = () => {
             <Route exact path='/' component={Home} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/signup' component={Signup} />
-            <Route exact path='/category' component={Category} />
-            <Route exact path='/login/recover' component={Recover} />\
+            <Route exact path='/category' component={Tag} />
+            <Route exact path='/login/recover' component={Recover} />
+            <Route exact path='/question' component={Question} />
             <Route path='*' component={NotFound} />
         </Switch> 
     )
