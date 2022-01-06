f8084fedc3eef5aded5d0294af195b088729acd9
pemba1s1 <norsangpemba@gmail.com>
2022-01-06 15:32:21 +0545

    moved to try catch

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -5,17 +5,17 @@ export const PrivateRoute = ({children}) => {
     let token = localStorage.getItem('token')
     if(token){
         try{
-            var tokenExp = jwtDecode(token).exp
+            let tokenExp = jwtDecode(token).exp
+            let dateNow = new Date()
+            if(tokenExp < dateNow.getTime()/1000){
+                localStorage.clear()
+                return <Redirect to='/login' />
+            }else{
+                return children
+            }
         }catch(error){
             localStorage.clear()
             return <Redirect to='/login' />
-        }
-        let dateNow = new Date()
-        if(tokenExp < dateNow.getTime()/1000){
-            localStorage.clear()
-            return <Redirect to='/login' />
-        }else{
-            return children
         }
     }else{
         return <Redirect to='/login' />
