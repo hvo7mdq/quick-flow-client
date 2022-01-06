03b8229496bb161818b2aabd030d5eba6db53464
pemba1s1 <norsangpemba@gmail.com>
2022-01-06 15:28:42 +0545

    try catch

--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -4,7 +4,12 @@ import jwtDecode from "jwt-decode";
 export const PrivateRoute = ({children}) => {
     let token = localStorage.getItem('token')
     if(token){
-        let tokenExp = jwtDecode(token).exp
+        try{
+            let tokenExp = jwtDecode(token).exp
+        }catch(error){
+            localStorage.clear()
+            return <Redirect to='/login' />
+        }
         let dateNow = new Date()
         if(tokenExp < dateNow.getTime()/1000){
             localStorage.clear()
