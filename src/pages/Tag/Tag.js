f96261a6f47927f3816906af0de97fb1425b948d
pemba1s1 <norsangpemba@gmail.com>
2022-01-10 16:43:17 +0545

    tags

--- src/pages/Tag/Tag.js
+++ src/pages/Tag/Tag.js
@@ -2,8 +2,13 @@ import React, { useEffect, useState } from 'react'
 import axiosInstance from '../../axios'
 import SingleTag from '../../components/Tag/SingleTag'
 import SecondaryLayout from '../../layout/SecondaryLayout'
+import { Formik,Form,Field } from 'formik'
+import { searchInitialValues } from '../../constants/Form/SearchInitialValues'
+import searchSchema from '../../schema/SearchSchema'
+import { Redirect, useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 export default function Tag() {
+    const history = useHistory()
     const [tags,setTag] = useState(null)
     useEffect(async ()=>{
         await axiosInstance.get('tags/').then(res=>{
@@ -13,10 +18,21 @@ export default function Tag() {
             // console.log(err)
         })
     },[])
+    const search = (values) => {
+        history.push(`tags/${values.search}`)
+    }
     return (
         <>
             <SecondaryLayout>            
             <p className='fw-bold fs-5'>Tags</p>
+            <Formik
+            initialValues={searchInitialValues}
+            onSubmit={search}
+            validationSchema={searchSchema}>
+                <Form className="d-flex w-25">
+                    <Field id="search" name="search" placeholder="Filter by tag name" className="form-control me-2" />
+                </Form>
+            </Formik>
             {tags ? 
             <SingleTag  tags={tags}/> 
             :
